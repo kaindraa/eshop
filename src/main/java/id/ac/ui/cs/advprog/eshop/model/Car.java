893bba12a30bd76f5404316e04bc60a6d62a22b5
kaindraa <pcgsachio@gmail.com>
2025-02-27 12:18:03 +0700

    Refactor: Add update() method in Car to comply with OCP, allowing future subclasses to override update logic without modifying core logic

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -9,4 +9,11 @@ public class Car {
     private String carName;
     private String carColor;
     private int carQuantity;
+
+    // Open for extension: Subclasses can override this method
+    public void update(Car updatedCar) {
+        this.carName = updatedCar.getCarName();
+        this.carColor = updatedCar.getCarColor();
+        this.carQuantity = updatedCar.getCarQuantity();
+    }
 }
