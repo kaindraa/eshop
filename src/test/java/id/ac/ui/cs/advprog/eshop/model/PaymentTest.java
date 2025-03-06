71125680b6b1d4fa5ab3ff2edf1db1980be7b35a
kaindraa <pcgsachio@gmail.com>
2025-03-06 16:34:45 +0700

    [RED] Make tests for Payment model

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -0,0 +1,65 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import org.junit.jupiter.api.Test;
+import java.util.Map;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class PaymentTest {
+
+
+    @Test
+    void testCreateValidPayment(){
+
+        Map<String, String> paymentData = new HashMap<>();
+        paymentData.put("voucherCode", "ESHOP12345678ABC");
+
+        payment = new Payment("3c011546-313a-4471-b897-8888e7b9cede",
+                "Voucher",
+                "REJECTED",
+                paymentData
+        );
+
+        assertEquals("3c011546-313a-4471-b897-8888e7b9cede", payment.getId());
+        assertEquals("Voucher", payment.getMethod());
+        assertEquals("REJECTED", payment.getStatus());
+        assertEquals("paymentData", payment.getPaymentData());
+
+    }
+
+    @Test
+    void testCreatePaymentInvalidMethod(){
+        Map<String, String> paymentData = new HashMap<>();
+        paymentData.put("voucherCode", "ESHOP12345678ABC");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            payment = new Payment("3c011546-313a-4471-b897-8888e7b9cede",
+                    "Voucher",
+                    "testing method",
+                    paymentData
+            );
+        });
+
+    }
+
+    @Test
+    void testCreatePaymentInvalidStatus(){
+        Map<String, String> paymentData = new HashMap<>();
+        paymentData.put("voucherCode", "ESHOP12345678ABC");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            payment = new Payment("3c011546-313a-4471-b897-8888e7b9cede",
+                    "Voucher",
+                    "test-status",
+                    paymentData
+            );
+        });
+
+
+
+
+
+    }
+}
