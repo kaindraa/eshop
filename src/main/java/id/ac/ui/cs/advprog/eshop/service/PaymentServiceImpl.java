0151f30da5692c8bb7e8c9ceda009c927555113f
kaindraa <pcgsachio@gmail.com>
2025-03-06 20:30:57 +0700

    [GREEN] Implement PaymentServiceImplTest

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -1,33 +1,69 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
-
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public class PaymentServiceImpl implements PaymentService {
 
     private PaymentRepository paymentRepository;
 
+    public PaymentServiceImpl(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
     @Override
     public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
-        return null;
+        Payment payment;
+        if (PaymentMethod.VOUCHER_CODE.getValue().equals(method)) {
+            String voucherCode = paymentData.get("voucherCode");
+            if (voucherCode != null && voucherCode.length() == 16 && voucherCode.startsWith("ESHOP")
+                    && voucherCode.chars().filter(Character::isDigit).count() == 8) {
+                payment = new Payment(UUID.randomUUID().toString(), method, PaymentStatus.SUCCESS.getValue(), paymentData);
+                order.setStatus(OrderStatus.SUCCESS.getValue());
+            } else {
+                payment = new Payment(UUID.randomUUID().toString(), method, PaymentStatus.REJECTED.getValue(), paymentData);
+                order.setStatus(OrderStatus.FAILED.getValue());
+            }
+        } else if (PaymentMethod.BANK_TRANSFER.getValue().equals(method)) {
+            String bankName = paymentData.get("bankName");
+            String referenceCode = paymentData.get("referenceCode");
+            if (bankName != null && !bankName.isEmpty() && referenceCode != null && !referenceCode.isEmpty()) {
+                payment = new Payment(UUID.randomUUID().toString(), method, PaymentStatus.SUCCESS.getValue(), paymentData);
+                order.setStatus(OrderStatus.SUCCESS.getValue());
+            } else {
+                payment = new Payment(UUID.randomUUID().toString(), method, PaymentStatus.REJECTED.getValue(), paymentData);
+                order.setStatus(OrderStatus.FAILED.getValue());
+            }
+        } else {
+            payment = new Payment(UUID.randomUUID().toString(), method, PaymentStatus.REJECTED.getValue(), paymentData);
+            order.setStatus(OrderStatus.FAILED.getValue());
+        }
+        return paymentRepository.save(payment);
     }
 
     @Override
     public Payment setStatus(Payment payment, String status) {
-        return null;
+        if (PaymentStatus.contains(status)) {
+            payment.setStatus(status);
+            return paymentRepository.save(payment);
+        }
+        return payment;
     }
 
     @Override
     public Payment getPayment(String paymentId) {
-        return null;
+        return paymentRepository.getPayment(paymentId);
     }
 
     @Override
     public List<Payment> getAllPayments() {
-        return null;
+        return paymentRepository.getAllPayments();
     }
 }
